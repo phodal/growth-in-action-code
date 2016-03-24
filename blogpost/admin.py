684073a937398c65d85a472fbf4cf6383fec055d
Fengda HUANG <h@phodal.com>
2016-03-24 12:48:02 +0800

    add blog post models

--- blogpost/admin.py
+++ blogpost/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from blogpost.models import Blogpost
 
-# Register your models here.
+class BlogpostAdmin(admin.ModelAdmin):
+    exclude = ['posted']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Blogpost, BlogpostAdmin)
