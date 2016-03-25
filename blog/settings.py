e16b7d268f2604090f2ccbce14f205682a82c180
Fengda HUANG <h@phodal.com>
2016-03-25 12:12:05 +0800

    fix path issues

--- blog/settings.py
+++ blog/settings.py
@@ -98,10 +98,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-PROJECT_APP_PATH = os.path.dirname(os.path.abspath(__file__))
-PROJECT_ROOT = BASE_DIR = os.path.dirname(PROJECT_APP_PATH)
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
-
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
-
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static/'),
+    '/statics/js/',
+    '/statics/css/',
+)
