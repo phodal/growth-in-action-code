0f1577110ad99550805821f5795484b9c15c9827
Fengda HUANG <h@phodal.com>
2016-03-25 14:51:37 +0800

    add service test

--- blogpost/tests.py
+++ blogpost/tests.py
@@ -1,3 +1,17 @@
+from django.core.urlresolvers import resolve
+from django.http import HttpRequest
 from django.test import TestCase
 
-# Create your tests here.
+from blogpost.views import index
+
+
+class HomePageTest(TestCase):
+    def test_root_url_resolves_to_home_page_view(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        response = index(request)
+        print(response.content)
+        self.assertIn(b'<title>Welcome to my blog</title>', response.content)
