602b3406890bd6b9a92d62858d2f2e5005aeca51
Fengda HUANG <h@phodal.com>
2016-03-25 16:48:23 +0800

    add unit test for blogpost

--- blogpost/tests.py
+++ blogpost/tests.py
@@ -1,8 +1,11 @@
+from datetime import datetime
+
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase
 
-from blogpost.views import index
+from blogpost.models import Blogpost
+from blogpost.views import index, view_post
 
 
 class HomePageTest(TestCase):
@@ -13,5 +16,9 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = index(request)
-        print(response.content)
         self.assertIn(b'<title>Welcome to my blog</title>', response.content)
+
+class BlogpostTest(TestCase):
+    def test_blogpost_url_resolves_to_home_page_view(self):
+        found = resolve('/blog/this_is_a_test.html')
+        self.assertEqual(found.func, view_post)
