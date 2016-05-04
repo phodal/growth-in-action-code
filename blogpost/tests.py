a091a2597735c328bc18aba1407452c9d50aed42
Fengda HUANG <h@phodal.com>
2016-05-04 23:50:44 +0800

    add test for homepage blogpost

--- blogpost/tests.py
+++ blogpost/tests.py
@@ -29,3 +29,9 @@ class BlogpostTest(TestCase):
                                 posted=datetime.now)
         response = self.client.get('/blog/this_is_a_test.html')
         self.assertIn(b'This is a blog', response.content)
+
+    def test_blogpost_create_with_show_in_homepage(self):
+        Blogpost.objects.create(title='hello', author='admin', slug='this_is_a_test', body='This is a blog',
+                                posted=datetime.now)
+        response = self.client.get('/')
+        self.assertIn(b'This is a blog', response.content)
