33e8a74040c5c4cd16626b08c41e8ab9fac2ec15
Fengda HUANG <h@phodal.com>
2016-05-04 23:47:27 +0800

    add function test for model

--- blogpost/tests.py
+++ blogpost/tests.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 from django.core.urlresolvers import resolve
 from django.http import HttpRequest
 from django.test import TestCase
 
+from blogpost.models import Blogpost
 from blogpost.views import index, view_post
 
 
@@ -15,7 +18,14 @@ class HomePageTest(TestCase):
         response = index(request)
         self.assertIn(b'<title>Welcome to my blog</title>', response.content)
 
+
 class BlogpostTest(TestCase):
     def test_blogpost_url_resolves_to_blog_post_view(self):
         found = resolve('/blog/this_is_a_test.html')
         self.assertEqual(found.func, view_post)
+
+    def test_blogpost_url_resolves_to_home_page_view(self):
+        Blogpost.objects.create(title='hello', author='admin', slug='this_is_a_test', body='This is a blog',
+                                posted=datetime.now)
+        response = self.client.get('/blog/this_is_a_test.html')
+        self.assertIn(b'This is a blog', response.content)
