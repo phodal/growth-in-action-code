591ddc22afc8b0e530049723ad2938646dd4722c
Fengda HUANG <h@phodal.com>
2016-05-12 01:55:46 +0800

    fix some permission issue

--- blog/settings.py
+++ blog/settings.py
@@ -117,7 +117,7 @@ JWT_AUTH = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
