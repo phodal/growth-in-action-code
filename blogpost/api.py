d0081b52c6a0a7ca9e8c5eac7e3be9db16e6ca58
Fengda HUANG <h@phodal.com>
2016-05-12 07:52:33 +0800

    add filter for user name

--- blogpost/api.py
+++ blogpost/api.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from blogpost.models import Blogpost
@@ -29,3 +30,11 @@ class UserDetail(viewsets.ReadOnlyModelViewSet):
     permission_classes = (permissions.IsAuthenticated,)
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def list(self, request):
+        search_param = self.request.query_params.get('username', None)
+        if search_param is not None:
+            queryset = User.objects.filter(username__contains=search_param)
+
+        serializer = UserSerializer(queryset, many=True)
+        return Response(serializer.data)
