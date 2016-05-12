8ca10449638c82f0d8c5d032a652dd045e98fedc
Fengda HUANG <h@phodal.com>
2016-05-12 21:10:44 +0800

    add IsAuthenticatedOrReadOnly permission rules

--- blogpost/api.py
+++ blogpost/api.py
@@ -1,11 +1,26 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
+from rest_framework.permissions import BasePermission
 from rest_framework.response import Response
 from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 
 from blogpost.models import Blogpost
 from rest_framework import permissions
+
+SAFE_METHODS = ['GET', 'HEAD', 'OPTIONS']
+
+class IsAuthenticatedOrReadOnly(BasePermission):
+    """
+    The request is authenticated as a user, or is a read-only request.
+    """
+
+    def has_permission(self, request, view):
+        if (request.method in SAFE_METHODS or
+            request.user and
+            request.user.is_authenticated()):
+            return True
+        return False
 
 class BlogpsotSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -15,6 +30,7 @@ class BlogpsotSerializer(serializers.HyperlinkedModelSerializer):
 
 # ViewSets define the view behavior.
 class BlogpostSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = Blogpost.objects.all()
     serializer_class = BlogpsotSerializer
 
