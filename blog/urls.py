69427072b6029cbaf56a4c5a5bb0ebeae30b10d1
Fengda HUANG <h@phodal.com>
2016-05-16 07:21:59 +0800

    fix some url waring

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,7 @@ from django.contrib.sitemaps.views import sitemap
 from rest_framework import routers
 
 from blogpost import views as blogpostViews
+from rest_framework_jwt import views as DRFViews
 from blogpost.api import BlogpostSet, UserDetail
 
 from sitemap.sitemaps import BlogSitemap, PageSitemap, FlatPageSitemap
@@ -22,14 +23,14 @@ sitemaps =  {
 
 urlpatterns = patterns('',
     url(r'^$', blogpostViews.index, name='main'),
-    url(r'^blog/(?P<slug>[^\.]+).html', 'blogpost.views.view_post', name='view_blog_post'),
+    url(r'^blog/(?P<slug>[^\.]+).html', blogpostViews.view_post, name='view_blog_post'),
     url(r'^comments/', include('django_comments.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(apiRouter.urls)),
     url(r'^pages/', include('django.contrib.flatpages.urls')),
     url(r'^sitemap\.xml$', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'^api-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
-    url(r'^api-token-verify/', 'rest_framework_jwt.views.verify_jwt_token'),
+    url(r'^api-token-auth/', DRFViews.obtain_jwt_token),
+    url(r'^api-token-refresh/', DRFViews.refresh_jwt_token),
+    url(r'^api-token-verify/', DRFViews.verify_jwt_token),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
